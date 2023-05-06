afbb30cd2b10e1bd7d16d09c27fd7fc6493f533e
Edwin Josué <54474913+EdwinJosue0124@users.noreply.github.com>
2023-05-05 21:23:16 -0600

    Borrado de archivos erroneos

--- app/src/main/java/com/example/proyecto_bienestarsv/ProveedorModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.proyecto_bienestarsv
-
-data class ProveedorModel (
-    var proveedorid: String? = null,
-    var nombre: String?=null,
-    var dirrecion: String?=null,
-    var telefono: String?=null,
-    var correo: String?=null,
-)
