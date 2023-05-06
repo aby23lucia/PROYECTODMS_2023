e1660507b7e9619a39858812cd2228d3e66020f8
Edwin Josué <54474913+EdwinJosue0124@users.noreply.github.com>
2023-05-05 21:35:55 -0600

    funcionamiento total de producto y parcial de proveedor

---
+++ app/src/main/java/com/example/proyecto_bienestarsv/ProductoModel.kt
@@ -0,0 +1,10 @@
+package com.example.proyecto_bienestarsv
+
+data class ProductoModel (
+    var empId: String? = null,
+    var empNomProducto: String?=null,
+    var empCantidad: String?=null,
+    var empMarca: String?=null,
+    var empProveedor: String?=null,
+    var empDescrpcion: String?=null
+)
